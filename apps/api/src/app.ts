0c80943033e360bc050c7572afa04764ac487f49
Vojtech Nerad <nerv01@vse.cz>
2026-05-20 21:02:30 +0200

    Not found handler

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -6,4 +6,8 @@ app.get('/', (c) => {
   return c.text('Hello Hono in Nx Monorepo!');
 });
 
+app.notFound((c) => {
+  return c.json({ message: `Path ${c.req.path} not found.` }, 404);
+});
+
 export default app;
