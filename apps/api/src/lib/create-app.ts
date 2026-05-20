ef006c2e161d89157b8bd8c9ef2ca42f28c139a2
Vojtech Nerad <nerv01@vse.cz>
2026-05-20 23:24:26 +0200

    Use helpers

--- apps/api/src/lib/create-app.ts
+++ apps/api/src/lib/create-app.ts
@@ -1,7 +1,22 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
+import { HttpStatusCodes } from '../helpers/http-status-codes.helper';
 
 export function createRouter() {
-  return new OpenAPIHono({ strict: true });
+  return new OpenAPIHono({
+    strict: true,
+    defaultHook: (result, c) => {
+      if (!result.success) {
+        const { success, error } = result;
+        return c.json(
+          {
+            success,
+            error,
+          },
+          HttpStatusCodes.UNPROCESSABLE_ENTITY,
+        );
+      }
+    },
+  });
 }
 
 export function createApp() {
--- apps/api/src/routes/index.route.ts
+++ apps/api/src/routes/index.route.ts
@@ -1,21 +1,19 @@
 import { createRoute, z } from '@hono/zod-openapi';
 import { createRouter } from '../lib/create-app';
+import { jsonContent } from '../helpers/openapi.helper';
+import { HttpStatusCodes } from '../helpers/http-status-codes.helper';
 
 const router = createRouter().openapi(
   createRoute({
     method: 'get',
     path: '/test',
     responses: {
-      200: {
-        content: {
-          'application/json': {
-            schema: z.object({
-              message: z.string(),
-            }),
-          },
-        },
-        description: 'Test',
-      },
+      [HttpStatusCodes.OK]: jsonContent(
+        z.object({
+          message: z.string(),
+        }),
+        'Test',
+      ),
     },
   }),
   (c) => {
