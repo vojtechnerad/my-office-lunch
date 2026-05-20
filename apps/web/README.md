baf98b9f09a9dc60e57fbe0b9d1027ee0cfcbd12
Vojtech Nerad <nerv01@vse.cz>
2026-05-20 19:14:24 +0200

    created empty web app

---
+++ apps/web/README.md
@@ -0,0 +1,12 @@
+Web app was created using following settings:
+
+```
+npx nx generate @nx/angular:application
+  --directory=apps/web
+  --addTailwind=true
+  --port=5173
+  --prefix=mol
+  --skipTests=true
+  --zoneless=true
+  --no-interactive
+```
---
+++ apps/web/src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  imports: [RouterModule],
+  selector: 'mol-root',
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {}
