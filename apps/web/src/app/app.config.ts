71b9b1fcb0406e88975f8a3d67e7786ecaac273e
Vojtech Nerad <vojtech.nerad2@gmail.com>
2026-05-25 22:14:52 +0200

    Add HTTP client configuration with auth interceptor to app config

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -4,7 +4,13 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(appRoutes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
