9bb18ac85c06185f8e58b973fc8f74931b460afe
Vojtech Nerad <vojtech.nerad2@gmail.com>
2026-06-14 04:44:29 +0200

    Group voting screen group info wip

--- apps/web/src/app/features/group-voting/pages/group-voting.ts
+++ apps/web/src/app/features/group-voting/pages/group-voting.ts
@@ -1,10 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../../core/services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'mol-group-voting',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './group-voting.html',
   styleUrl: './group-voting.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GroupVoting {}
+export class GroupVoting implements OnInit {
+  protected readonly group = signal<object | null>(null);
+
+  private readonly route = inject(ActivatedRoute);
+  private readonly apiService = inject(ApiService);
+
+  ngOnInit() {
+    const groupId = this.route.snapshot.paramMap.get('groupId');
+
+    if (groupId) {
+      this.apiService.getGroupById(groupId).subscribe((group) => {
+        this.group.set(group);
+      });
+    }
+  }
+}
