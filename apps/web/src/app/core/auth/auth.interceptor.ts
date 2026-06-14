8b9ca0e51ac02e691eba271a19b75abc14f70f98
Vojtech Nerad <vojtech.nerad2@gmail.com>
2026-06-14 19:32:41 +0200

    Add error handling to auth interceptor for unauthorized responses

--- apps/web/src/app/core/auth/auth.interceptor.ts
+++ apps/web/src/app/core/auth/auth.interceptor.ts
@@ -1,6 +1,10 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from './auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
   const token = localStorage.getItem('token');
 
   if (token) {
@@ -11,5 +15,14 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     });
   }
 
-  return next(req);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      // If the error status is 401 (Unauthorized), sign out the user
+      if (error.status === 401) {
+        authService.signOut();
+      }
+
+      return throwError(() => error);
+    }),
+  );
 };
