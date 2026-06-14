0571afe36b26e46d23f3e771562b4ca78d7305d4
Vojtech Nerad <vojtech.nerad2@gmail.com>
2026-06-14 02:52:32 +0200

    Add GroupVoting component with routing and template files

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Auth } from './features/auth/auth';
 import { authGuard } from './core/auth/auth.guard';
 import { MainLayout } from './layouts/main-layout/main-layout';
 import { Groups } from './features/groups/groups';
+import { GroupVoting } from './features/group-voting/pages/group-voting';
 
 export const appRoutes: Route[] = [
   { path: 'auth', component: Auth },
@@ -12,10 +13,8 @@ export const appRoutes: Route[] = [
     canActivate: [authGuard],
     component: MainLayout,
     children: [
-      {
-        path: 'groups',
-        component: Groups,
-      },
+      { path: 'group-voting/:groupId', component: GroupVoting },
+      { path: 'groups', component: Groups },
     ],
   },
 
---
+++ apps/web/src/app/features/group-voting/pages/group-voting.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'mol-group-voting',
+  imports: [],
+  templateUrl: './group-voting.html',
+  styleUrl: './group-voting.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GroupVoting {}
