017fda5cf659fb1b2fdd556b95f6c64d5d0bb227
Vojtech Nerad <vojtech.nerad2@gmail.com>
2026-06-29 14:12:59 +0200

    feat: refactor main layout to use HLM components and improve sidebar structure

--- apps/web/src/app/layouts/main-layout/main-layout.ts
+++ apps/web/src/app/layouts/main-layout/main-layout.ts
@@ -7,27 +7,51 @@ import {
 } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../core/auth/auth.service';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
-import { NzSpaceModule } from 'ng-zorro-antd/space';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { ApiService } from '../../core/services/api.service';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzDropdownModule } from 'ng-zorro-antd/dropdown';
+import { HlmSidebarImports } from '@spartan-ng/helm/sidebar';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  lucideCalendar,
+  lucideChevronsUpDown,
+  lucideCookingPot,
+  lucideHouse,
+  lucideInbox,
+  lucideLogOut,
+  lucideSearch,
+  lucideSettings,
+  lucideUser,
+  lucideUsers,
+} from '@ng-icons/lucide';
+import { HlmDropdownMenuImports } from '@spartan-ng/helm/dropdown-menu';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmAvatarImports } from '@spartan-ng/helm/avatar';
+import { HlmSkeletonImports } from '@spartan-ng/helm/skeleton';
 
 @Component({
   selector: 'mol-main-layout',
   imports: [
     RouterOutlet,
-    NzButtonModule,
-    NzDropdownModule,
-    NzIconModule,
-    NzLayoutModule,
-    NzPageHeaderModule,
-    NzSpaceModule,
-    NzMenuModule,
     RouterModule,
+    HlmSidebarImports,
+    HlmButtonImports,
+    HlmDropdownMenuImports,
+    HlmAvatarImports,
+    NgIcon,
+    HlmSkeletonImports,
+  ],
+  providers: [
+    provideIcons({
+      lucideCookingPot,
+      lucideHouse,
+      lucideInbox,
+      lucideCalendar,
+      lucideSearch,
+      lucideSettings,
+      lucideUser,
+      lucideUsers,
+      lucideChevronsUpDown,
+      lucideLogOut,
+    }),
   ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
@@ -35,6 +59,7 @@ import { NzDropdownModule } from 'ng-zorro-antd/dropdown';
 })
 export class MainLayout implements OnInit {
   protected myGroups = signal<Array<{ id: string; name: string }>>([]);
+  protected isGroupsLoading = signal(true);
   protected name = localStorage.getItem('name') || '';
 
   private authService = inject(AuthService);
@@ -47,6 +72,7 @@ export class MainLayout implements OnInit {
   ngOnInit(): void {
     this.apiService.myGroups().subscribe((groups) => {
       this.myGroups.set(groups);
+      this.isGroupsLoading.set(false);
     });
   }
 }
